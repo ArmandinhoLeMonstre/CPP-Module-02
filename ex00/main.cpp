8c9c5bb70575a60dcc6c9e194ac443292dd2bdf8
Armand Mititelu <armitite@mi-c2-p7.s19.be>
2025-02-16 17:26:10 +0100

    Makefile

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: armitite <armitite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:27:20 by armitite          #+#    #+#             */
-/*   Updated: 2025/02/05 19:48:17 by armitite         ###   ########.fr       */
+/*   Updated: 2025/02/16 17:02:37 by armitite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int main(void)
 	Fixed c;
 	c = b;
 
-	std::cout << a.getRawBits() << std::endl;
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
